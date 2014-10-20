cf4507b6880336e43f30565d169511dbcd1431a7
BragaLuis <Seixas.luis.miguel@gmail.com>
2014-10-20 09:53:35 +0200

    [Environnement] .gitignore
    configuration du .gitignore

--- Site/script/EventCalendar.php
+++ Site/script/EventCalendar.php
@@ -4,7 +4,7 @@
 * Avec connection a la bdd
 *
 */
-
+session_start();
 include('../config/config.php');
 
 $start = $_REQUEST['from'] / 1000;
@@ -15,15 +15,17 @@ $end   = $_REQUEST['to'] / 1000;
 		$sql="SELECT * FROM seances 
 		left join (seances_profs) on (seances.codeSeance=seances_profs.codeSeance ) 
 		left join (enseignements) on (seances.codeEnseignement=enseignements.codeEnseignement) 
-		right join (matieres) on (matieres.codeMatiere=enseignements.codeMatiere) 
-		where seances_profs.deleted='0'
+		right join (matieres) on (matieres.codeMatiere=enseignements.codeMatiere)
+                left join login_prof lp on lp.codeprof = enseignements.codeProprietaire
+		where lp.codeProf = '".$_SESSION['teachLogin']."'
+		AND seances_profs.deleted='0'
 		AND seances.deleted='0' 
 		and matieres.deleted='0' 
 		and enseignements.deleted='0' 
 		AND seances.annulee='0'  
 		";
-	
-
+	*/
+/*
 	$req_seances_prof=$dbh->prepare($sql);
 	
 	//requete pour avoir les groupes associés aux séances	
@@ -110,15 +112,122 @@ $total_min_eqtd_module='';
 		order by matieres.nom,seances.dateSeance,seances.heureSeance";
 	
 */
-$sql   = sprintf('SELECT * from seances where dateModif='.$dbh->quote('2014-09-22 13:45:28', PDO::PARAM_STR));  //2014-09-22 13:45:28');//('SELECT * FROM events WHERE `datetime` BETWEEN %s and %s',
-$req = $dbh->prepare($sql);
-//print_r($req);
-$req->execute();
+//$loginUtilisateur = "";
+	
+	/*if (isset($_SESSION['teachLogin']))
+	{
+		$loginUtilisateur = $_SESSION['teachLogin'];
+                echo $loginUtilisateur;
+	}
+	else
+	{
+		$loginUtilisateur = $_COOKIE['teachLogin'];
+                echo $loginUtilisateur;
+	}*/
 
-$out = array();
+	/*$userName= "";
+	$firstName= "";
 
-$data = getdate();//'Y-m-d', strtotime("+14 days"));
-//print_r($data[0]);
+	// on récupere le codeProf reliés au login de l'enseignant
+	$sql = "SELECT * FROM login_prof WHERE login= ".$dbh->quote($loginUtilisateur, PDO::PARAM_STR);   
+	$req = $dbh->prepare($sql);
+	$req->execute();
+	$codeProf = 0;
+		  
+	// Si oui, on continue le script...      
+	while($ligneCode = $req->fetch())
+	{
+		$codeProf = $ligneCode['codeProf'];
+	}
+	$req->closeCursor();*/
+   
+   /*
+ $sql="SELECT nom, prenom from seances_profs left join ressources_profs on (seances_profs.codeRessource=ressources_profs.codeProf) WHERE seances_profs.codeSeance=".$code_seance." and seances_profs.deleted='0' and ressources_profs.deleted='0' order by ressources_profs.nom"; 
+			$req5=$dbh->prepare($sql);
+			$req5->execute();
+			$premierProf = TRUE;
+			
+			while($res_5 = $req5->fetch())
+			{
+				$prenom=ucwords(strtolower($res_5['prenom'])) ;
+				if($premierProf == FALSE)
+				{
+					$nom_prof .= " - ";
+				}
+				$nom_prof .= $prenom." ".$res_5['nom'];
+				$premierProf = FALSE;
+			}
+			$req5->closeCursor();  
+   */
+   
+   
+ /*  
+   
+$sql="SELECT * FROM seances 
+		left join (seances_profs) on (seances.codeSeance=seances_profs.codeSeance ) 
+		left join (enseignements) on (seances.codeEnseignement=enseignements.codeEnseignement) 
+		right join (matieres) on (matieres.codeMatiere=enseignements.codeMatiere)
+                left join login_prof lp on lp.codeprof = enseignements.codeProprietaire
+		where lp.codeProf = ".$teachLogin."
+		AND seances_profs.deleted='0'
+		AND seances.deleted='0' 
+		and matieres.deleted='0' 
+		and enseignements.deleted='0' 
+		AND seances.annulee='0'  
+";
+*/
+ 
+    $loginUtilisateur = "";
+           
+        if (isset($_SESSION['teachLogin']))
+        {
+            $loginUtilisateur = $_SESSION['teachLogin'];
+        }
+        else
+        {
+            $loginUtilisateur = $_COOKIE['teachLogin'];
+        }
+   
+   
+        // on récupere le codeProf reliés au login de l'enseignant
+        //$sql = "SELECT * FROM login_prof WHERE login= ".$dbh->quote($loginUtilisateur, PDO::PARAM_STR);
+        $sql="SELECT seances.dateSeance, seances.heureSeance, seances.dureeSeance, enseignements.nom, enseignements.dureeSeance, enseignements.couleurFond, enseignements.alias, enseignements.codeTypeSalle, matieres.couleurFond, matieres.nom, login_prof.login
+                FROM seances
+                LEFT JOIN seances_profs ON seances.codeSeance = seances_profs.codeSeance
+                LEFT JOIN enseignements ON seances.codeEnseignement = enseignements.codeEnseignement
+                RIGHT JOIN matieres ON matieres.codeMatiere = enseignements.codeMatiere
+                LEFT JOIN login_prof ON login_prof.codeprof = enseignements.codeProprietaire
+                WHERE seances_profs.deleted =  '0'
+                AND seances.deleted =  '0'
+                AND matieres.deleted =  '0'
+                AND matieres.deleted =  '0'
+                AND seances.annulee =  '0'
+                AND login_prof.login = ".$dbh->quote($loginUtilisateur, PDO::PARAM_STR);
+        $req = $dbh->prepare($sql);
+        $req->execute(); 
+        $out = array();
+        
+        $data = getdate();//'Y-m-d', strtotime("+14 days"));
+        print_r($req);
+        
+        /*if()
+        {
+            
+        }
+        else if()
+        {
+            
+        }
+        else if()
+        {
+            
+        }
+        else if()
+        {
+            
+        }*/
+
+
 while($ligneCode = $req->fetch()) {    
 	$out[] = array(
         'id' => $ligneCode['codeSeance'],
