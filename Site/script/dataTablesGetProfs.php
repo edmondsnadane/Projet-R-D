48ca82bdf2b5cf2be73c3ca6e7aa3c14f8da1645
Indy <dev@indydedeken.fr>
2014-10-20 13:26:53 +0200

    Revert "[VT2 bis] Débug de la recherche d'enseignants"
    This reverts commit a6956bc634c8fa2ac68c9eb69763c7ef575e76bf.

--- Site/script/dataTablesGetProfs.php
+++ Site/script/dataTablesGetProfs.php
@@ -6,33 +6,14 @@
 
 include( "../config/config.php" );
 
-    /*
-     *
-     * Sécuriser cette page, limiter l'accès...
-     *
-     **/
+$deleteValue = 0;
+$data        = array(':deleteValue'=>$deleteValue);
 
+$sql           = "SELECT codeProf, nom, prenom FROM $dernierebase.ressources_profs WHERE deleted=:deleteValue;";
+$req_listeProf = $dbh->prepare($sql);
+$req_listeProf->execute($data);
+$res_listeProf = $req_listeProf->fetchAll();
 
-    // si on fait une recherche précise
-    if (isset($_GET["search"]["value"]) && $_GET["search"]["value"] != '') {
-        // construction de la clause where
-        $whereClause = "AND ";
-        $whereClause .= "(nom LIKE '%" . $_GET["search"]["value"] . "%' ";
-        $whereClause .= "OR prenom LIKE '%" . $_GET["search"]["value"] . "%' ";
-        $whereClause .= ")";
-    } else {
-        // cas où GET est vide/null
-        $whereClause = "";
-    }
+$data  = array("data" => $res_listeProf);
 
-    $deleteValue = 0;
-    $data        = array(':deleteValue'=>$deleteValue);
-
-    $sql           = "SELECT codeProf, nom, prenom FROM $dernierebase.ressources_profs WHERE deleted=:deleteValue $whereClause ";
-    $req_listeProf = $dbh->prepare($sql);
-    $req_listeProf->execute($data);
-    $res_listeProf = $req_listeProf->fetchAll();
-
-    $data  = array("data" => $res_listeProf);
-
-    echo json_encode($data);
+echo json_encode($data);
