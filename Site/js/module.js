aac4c030f3bfe479182a49251aea595ebb6f6666
Pierrick Moreau <pierrick.moreau.etud@hotmail.fr>
2014-10-28 11:05:10 +0100

    Avancement script export PDF

--- Site/js/module.js
+++ Site/js/module.js
@@ -1,10 +1,13 @@
-function loadProfsList() {
+function loadProfsList()
+{
     var codeComposante = $('#departements :selected').val();
 
-    addProfsToOptions = function(profs) {
+    addProfsToOptions = function(profs)
+	{
         $('#profs').empty();
         if (profs.length) {
-            for (i = 0; i < profs.length; i++) {
+            for (i = 0; i < profs.length; i++)
+			{
                 profInfos = profs[i].split("#");
                 $('#profs').append("<option value=" + profInfos[0] + ">" + profInfos[1] + " " + profInfos[2] + "</option>");
             }
@@ -20,23 +23,28 @@ function loadProfsList() {
         },
         cache: false,
         dateType: 'text',
-        success: function(data) {
-            console.log(data);
+        success: function(data)
+		{
             addProfsToOptions(data.split("~"));
         },
-        error: function(data) {
+        error: function(data)
+		{
             alert(data);
         }
     });
 }
 
-function loadModuleList() {
+function loadModuleList()
+{
     var codeProf = $('#profs :selected').val();
 
-    addModulesToOptions = function(module) {
+    addModulesToOptions = function(module)
+	{
         $('#module').empty();
-        if (module.length) {
-            for (i = 0; i < module.length; i++) {
+        if (module.length)
+		{
+            for (i = 0; i < module.length; i++)
+			{
                 $('#module').append("<option>" + module[i] + "</option>");
             }
             loadSeanceList();
@@ -46,59 +54,78 @@ function loadModuleList() {
     $.ajax({
         type: "POST",
         url: "./script/getTeachModule.php",
-        data: {
-            code: codeProf
-        },
+        data: { code: codeProf },
         cache: false,
         dateType: 'text',
-        success: function(data) {
+        success: function(data)
+		{
             addModulesToOptions(data.split("~"));
         },
-        error: function(data) {
+        error: function(data)
+		{
             alert(data);
         }
     });
 }
 
-function loadSeanceList() {
+function loadSeanceList()
+{
     var codeModule = $('#module :selected').text();
 
     /* fonction recuperant la liste des groupe dans lequel n'est pas un utilisateur */
-    createSeanceTable = function(seance) {
+    createSeanceTable = function(seance)
+	{
         $('#tableContent').empty();
-        if (seance.length) {
-            for (i = 0; i < seance.length; i++) {
+        if (seance.length)
+		{
+            for (i = 0; i < seance.length; i++)
+			{
                 var ligne = "<tr>";
                 var seanceInfo = seance[i].split("#");
-                for (j = 0; j < seanceInfo.length; j++) {
-                    ligne += "<td ";
-                    if (j == 2) {
-                        if (seanceInfo[j] == "CM") {
-                            ligne += "class='info'";
-                        } else if (seanceInfo[j] == "TD") {
-                            ligne += "class='success'";
-                        } else if (seanceInfo[j] == "TP") {
-                            ligne += "class='warning'";
-                        } else {
-                            ligne += "class='danger'";
-                        }
-                    }
+                for (j = 0; j < seanceInfo.length; j++)
+				{
+					ligne += "<td ";
+					if (j == 2)
+					{
+						if (seanceInfo[j] == "CM")
+						{
+							ligne += "class='info'";
+						}
+						else if (seanceInfo[j] == "TD")
+						{
+							ligne += "class='success'";
+						}
+						else if (seanceInfo[j] == "TP")
+						{
+							ligne += "class='warning'";
+						}
+						else 
+						{
+							ligne += "class='danger'";
+						}
+					}
 
-                    ligne += ">";
-                    if (j == (seanceInfo.length - 1)) {
-                        if (seanceInfo[j] == 1) {
-                            ligne += "<span class='glyphicon glyphicon-ok-circle'></span>";
-                        }
-                    } else {
-                        ligne += seanceInfo[j];
-                    }
-                    ligne += "</td>";
-                }
+					ligne += ">";
+					if (j == (seanceInfo.length - 1))
+					{
+						if (seanceInfo[j] == 1)
+						{
+							ligne += "<span class='glyphicon glyphicon-ok-circle'></span>";
+						}
+					}
+					else
+					{
+						ligne += seanceInfo[j];
+					}
+					ligne += "</td>";
+				}
                 ligne += "</tr>";
 
                 $('#tableContent').append(ligne);
             }
-        } else {
+        }
+		else
+		{
             $('#tableContent').append("<tr class='danger'><td colspan=9>Aucun resultats trouvés</td></tr>");
         }
     };
@@ -106,17 +133,11 @@ function loadSeanceList() {
     $.ajax({
         type: "POST",
         url: "./script/getSeanceByUserAndModule.php",
-        data: {
-            module: codeModule
-        },
+        data: { module: codeModule },
         cache: false,
         dateType: 'text',
-        success: function(data) {
-            createSeanceTable(data.split("~"));
-        },
-        error: function(data) {
-            alert(data);
-        }
+        success: function(data) { createSeanceTable(data.split("~"));},
+        error: function(data) {alert(data);}
     });
 
     return false;
