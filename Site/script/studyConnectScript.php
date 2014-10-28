09103a7d72c7757759308b953c2791a25604d4b2
Pierrick Moreau <pierrick.moreau.etud@hotmail.fr>
2014-10-28 08:52:55 +0100

    Revert "[Login] Ajaxification du login teach + study"
    This reverts commit ce2e7983ada7f4b465908ed0f6b41698d3807454.

--- Site/script/studyConnectScript.php
+++ Site/script/studyConnectScript.php
@@ -4,10 +4,6 @@ session_start();
 
 include('../config/config.php');
 
-// tableau suivant l'état de la connexion
-$tableau = array("message"	 => "En attente",
-				"connexion" => false);
-
 if (isset($_POST['studyLogin']) && !empty($_POST['studyLogin']))
 {
 	$find = FALSE;
@@ -16,38 +12,36 @@ if (isset($_POST['studyLogin']) && !empty($_POST['studyLogin']))
 	$sql = "SELECT * FROM ressources_etudiants WHERE identifiant=".$dbh->quote($_POST['studyLogin'], PDO::PARAM_STR)." AND deleted=0";
 	$req = $dbh->prepare($sql);
 	$req->execute();
-
-	// Si oui, on continue le script...
+		  
+	// Si oui, on continue le script...      
 	while($find == FALSE && $ligne = $req->fetch())
 	{
 		$find = TRUE;
-
+			
 		$sql="UPDATE compteur SET valeur=valeur+1 WHERE id_compteur='1'";
 		$dbh->exec($sql);
 	}
 
 	$req->closeCursor();
-
+				
 	// Sinon on lui affiche un message d'erreur.
 	if($find == FALSE)
 	{
-		$tableau["message"]	  = "Connexion refusée";
-		$tableau["connexion"] = false;
+		header('Location: ../index.php?errorID=2');
+		exit();
 	}
 	else
 	{
 		$_SESSION['studyLogin'] = $_POST['studyLogin'];
-
-		$tableau["message"]	  	= "Connexion en cours";
-		$tableau["connexion"] 	= true;
 	}
-
-	echo json_encode($tableau);
-
+	
+	header('Location: ../index.php');
+	exit();
 }
 else
 {
-	echo json_encode($tableau);
+	header('Location: ../index.php?errorID=1');
+	exit();
 }
 
 ?>
