a0fada2ea132eadc0191459a9199512d3d654d0d
Pierrick Moreau <pierrick.moreau.etud@hotmail.fr>
2014-11-17 15:52:55 +0100

    correction admin.js

--- Site/js/admin.js
+++ Site/js/admin.js
@@ -1,5 +1,16 @@
 function displayDroits()
 {
+		function manageDroitState(id, state)
+		{
+			if (state == 1)
+			{
+				$(id).addClass("active");
+			}
+			else
+			{
+				$(id).removeClass("active");
+			}
+		}
 		var codeProf = $('#profs :selected').val();
 		
 		$.ajax({
@@ -12,122 +23,20 @@ function displayDroits()
 		})
 		.done(function(elem)
 		{
-			if (elem.admin == 1)
-			{
-				$("#admin").addClass("active");
-			}
-			else
-			{
-				$("#admin").removeClass("active");
-			}
-			
-			if (elem.giseh == 1)
-			{
-				$("#giseh").addClass("active");
-			}
-			else
-			{
-				$("#giseh").removeClass("active");
-			}
-			
-			if (elem.bilan_formation == 1)
-			{
-				$("#bilan_formation").addClass("active");
-			}
-			else
-			{
-				$("#bilan_formation").removeClass("active");
-			}
-			
-			if (elem.salle == 1)
-			{
-				$("#bilan_salle").addClass("active");
-			}
-			else
-			{
-				$("#bilan_salle").removeClass("active");
-			}
-			
-			if (elem.mes_droits == 1)
-			{
-				$("#droits").addClass("active");
-			}
-			else
-			{
-				$("#droits").removeClass("active");
-			}
-			
-			if (elem.bilan_heure_global == 1)
-			{
-				$("#bilan_heure").addClass("active");
-			}
-			else
-			{
-				$("#bilan_heure").removeClass("active");
-			}
-			
-			if (elem.pdf == 1)
-			{
-				$("#pdf").addClass("active");
-			}
-			else
-			{
-				$("#pdf").removeClass("active");
-			}
-			
-			if (elem.rss == 1)
-			{
-				$("#rss").addClass("active");
-			}
-			else
-			{
-				$("#rss").removeClass("active");
-			}
-			
-			if (elem.configuration == 1)
-			{
-				$("#config").addClass("active");
-			}
-			else
-			{
-				$("#config").removeClass("active");
-			}
-			
-			if (elem.reservation == 1)
-			{
-				$("#reservation").addClass("active");
-			}
-			else
-			{
-				$("#reservation").removeClass("active");
-			}
-	
-			if (elem.module == 1)
-			{
-				$("#modules").addClass("active");
-			}
-			else
-			{
-				$("#modules").removeClass("active");
-			}
-			
-			if (elem.dialogue == 1)
-			{
-				$("#dialogue").addClass("active");
-			}
-			else
-			{
-				$("#dialogue").removeClass("active");
-			}
-			
-			if (elem.bilan_heure == 1)
-			{
-				$("#heures").addClass("active");
-			}
-			else
-			{
-				$("#heures").removeClass("active");
-			}
+			manageDroitState("#admin", elem.admin);
+			manageDroitState("#giseh", elem.giseh);
+			manageDroitState("#bilan_formation", elem.bilan_formation);
+			manageDroitState("#bilan_salle", elem.salle);
+			manageDroitState("#droits", elem.mes_droits);
+			manageDroitState("#bilan_salle", elem.salle);
+			manageDroitState("#bilan_heure", elem.bilan_heure_global);
+			manageDroitState("#pdf", elem.pdf);
+			manageDroitState("#rss", elem.rss);
+			manageDroitState("#config", elem.configuration);
+			manageDroitState("#reservation", elem.reservation);
+			manageDroitState("#modules", elem.module);
+			manageDroitState("#dialogue", elem.dialogue);
+			manageDroitState("#heures", elem.bilan_heure);
 		})
 		.fail(function(elem)
 		{
