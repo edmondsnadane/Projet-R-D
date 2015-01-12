af974f5751a3a0bfd2fa56a7223ec7982830ac63
BragaLuis <Seixas.luis.miguel@gmail.com>
2015-01-12 12:06:53 +0100

    [CALENDAR] Panel Filtre effet de transition
    ajout d'un effetàa l'affichage et au masquage du panel

--- Site/js/MobileSwipeCalendar.js
+++ Site/js/MobileSwipeCalendar.js
@@ -19,7 +19,9 @@
             panel.animate({
                 left: "0px"
               }, 500 );
-            calendarContainer.css({"opacity": "0.5"});
+            calendarContainer.animate({
+                opacity: "0.5"
+              }, 500 );
             count = 1;
         }
         else
@@ -27,7 +29,22 @@
             panel.animate({
                 left: "-300px"
               }, 500 );
-            calendarContainer.css({"opacity": "1"});
+            calendarContainer.animate({
+                opacity: "1"
+              }, 500 );
+            count = 0;
+        }
+    });
+    
+    $(document.body).click(function(e) {
+        // Si ce n'est pas #boutonPanel ni un de ses enfants
+        if( !$(e.target).is(boutonPanel) && !$.contains(boutonPanel[0],e.target) ) {
+            panel.animate({
+                left: "-300px"
+              }, 500 );
+            calendarContainer.animate({
+                opacity: "1"
+              }, 500 );
             count = 0;
         }
     });
